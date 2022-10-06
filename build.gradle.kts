1e9ba02b4aafddbea89e2d1c4db7c56c00924091
Joon Hee Song <mike.urssu@gmail.com>
2022-10-06 15:09:55 +0900

    Logging for Java and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java")
+    kotlin("jvm") version "1.7.20-RC"
 }
 
 group = "org.example"
@@ -10,10 +13,29 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+
+    // logging for java
+    implementation("org.slf4j:slf4j-api:2.0.1")
+    implementation("ch.qos.logback:logback-classic:1.4.1") {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+        exclude(group = "ch.qos.logback", module = "logback-core")
+    }
+    implementation("ch.qos.logback:logback-core:1.4.1")
+
+    // logging for kotlin
+    // https://github.com/MicroUtils/kotlin-logging
+    // Important note: kotlin-logging depends on slf4j-api (in the JVM artifact).
+    //                 In runtime, it is also required to depend on a logging implementation
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "11"
 }
