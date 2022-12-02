bf1ca7cc1f110172c3836cec46e365e85cac086e
Joon Hee Song <mike.urssu@gmail.com>
2022-12-02 13:42:47 +0900

    Gradle: 버전 update

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,30 +17,23 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
-    // logging for java
-    implementation("org.slf4j:slf4j-api:2.0.1")
-    implementation("ch.qos.logback:logback-classic:1.4.1") {
+    // log
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("ch.qos.logback:logback-classic:1.4.5") {
         exclude(group = "org.slf4j", module = "slf4j-api")
         exclude(group = "ch.qos.logback", module = "logback-core")
     }
-    implementation("ch.qos.logback:logback-core:1.4.1")
+    implementation("ch.qos.logback:logback-core:1.4.5")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
 
-    // logging for kotlin
-    // https://github.com/MicroUtils/kotlin-logging
-    // Important note: kotlin-logging depends on slf4j-api (in the JVM artifact).
-    //                 In runtime, it is also required to depend on a logging implementation
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
-
-
-    { // Excel Util
-        implementation("org.apache.commons:commons-collections4:4.4")
-        implementation("org.apache.poi:poi:5.2.3")
-        implementation("org.apache.poi:poi-ooxml:5.2.3")
-        implementation("org.apache.poi:poi-ooxml-lite:5.2.3")
-        implementation("org.apache.xmlbeans:xmlbeans:5.1.1")
-        implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-        implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    }
+    // Excel Util
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("org.apache.poi:poi-ooxml-lite:5.2.3")
+    implementation("org.apache.xmlbeans:xmlbeans:5.1.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
 }
 
 tasks.getByName<Test>("test") {
